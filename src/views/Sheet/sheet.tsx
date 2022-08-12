382ba0644e0a631d55978840a8cb15af3189b7a6
nymeku <arnaud.andrieu@epitech.eu>
2022-08-12 17:42:29 +0200

    Add Logo

--- src/views/Sheet/sheet.tsx
+++ src/views/Sheet/sheet.tsx
@@ -1,4 +1,13 @@
+import Logo from "../../components/Logo/logo"
 import "./sheet.scss"
 export default function Sheet() {
-	return <div className="Layout"></div>
+	return (
+		<div className="Layout">
+			<div className="LeftContainer">
+				<Logo />
+			</div>
+
+			<div className="RightContainer"></div>
+		</div>
+	)
 }
